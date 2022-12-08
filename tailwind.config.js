40f6a0794583502c0400883352168686fd3dd3a0
zaharich09 <97597336+zaharichswiftly@users.noreply.github.com>
2022-12-08 07:44:49 +0300

    upd 0.9v

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors:{
+        pink: "#ff79c6",
+        purple: "#bd93f9",
+        comment: "#282a36",
+        cyan: "#8be9fd",
+      },
       fontFamily: {
         burtons: "burtons",
+        montserrat: "montserrat",
       },
     },
   },
